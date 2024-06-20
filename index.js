ccc02656b17cef2d22d2968f6a08aa35ff8dcbb4
EZHILARASI K <108933770+ezhilk14@users.noreply.github.com>
2024-06-20 19:26:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./c:/Users/Ezhilarasi/Desktop/GUVI/ReactJs/react_component/src/app";
+
+ReactDOM.render(
+ 
+    <App />,
+ 
+  document.getElementById('root')
+);
